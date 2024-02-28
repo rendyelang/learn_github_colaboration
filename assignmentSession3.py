8d65d85e00fc972b44aaa71b1f297c72c0e88ef0
rendyelang <lsmnrndy123@gmail.com>
2024-02-28 14:40:06 +0700

    tambah file assignment#3

---
+++ assignmentSession3.py
@@ -0,0 +1,8 @@
+name = "Rendy"
+kelas = "TI23I"
+major = "Informatics"
+quote = "You don't have to be the gretest, just be better than yesterday"
+
+print("My name is", name)
+print("I am in class", kelas, "in majoring", major)
+print("Queote of the day: ", quote)
