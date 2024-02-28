89b02052c1b111601eae37afda480e0bfefd2c3a
rendyelang <lsmnrndy123@gmail.com>
2024-02-28 14:19:11 +0700

    buat program hitung luas

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+panjang = 10.0
+lebar = 5
+luas = panjang * lebar
+print("Panjang : ", panjang, ". Lebar : ", lebar, ". Luas : ", luas)
+print("Tipe data dari panjang:", type(panjang))
+print("Tipe data dari lebar:", type(lebar))
+print("Tipe data dari luas:", type(luas))
